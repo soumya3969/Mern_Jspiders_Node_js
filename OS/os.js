71b085789d6519c63b308e5a71058858bb10783d
soumya3969 <srmhnt2001@gmail.com>
2024-04-04 13:00:20 +0530

    os and fs added

---
+++ OS/os.js
@@ -0,0 +1,12 @@
+const os = require('os');
+console.log(os.machine()) // return ...
+console.log(os.arch()) // return computer architecture
+console.log(os.hostname()) // return os host name
+console.log(os.platform()) // return os platform
+console.log(os.tmpdir()) // return the os default directory for temp files
+console.log(os.type()) // return the type of os
+console.log(os.userInfo()) // return the current user info as a object
+console.log(os.freemem()) // return the free available memory
+console.log(os.totalmem()) // return the total memory
+console.log(os.homedir()) // return the home directory
+console.log(os.uptime()) //
